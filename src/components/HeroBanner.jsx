3c3e78481ba8b48f7daf6bb724b6c26d95a5e1a2
Your NameMueez <mueezimran1@gmail.com>
2025-07-26 17:40:28 +0500

    img fix

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
 function HeroBanner() {
-  const imageUrl =
-    'https://i.pinimg.com/736x/0f/1a/4b/0f1a4b59be2631957536b051ca735c5f.jpg';
+  const imageUrl = '/public/logo.png'; // path relative to the public folder
 
   return (
     <div className="relative">
-<div
-  className="min-h-[240px] md:min-h-[480px] bg-cover lg:bg-contain bg-center bg-no-repeat transition-all duration-500 ease-in-out"
-  style={{
-    backgroundImage: `url("${imageUrl}")`,
-  }}
-/>
-
+      <div
+        className="min-h-[240px] md:min-h-[480px] bg-cover lg:bg-contain bg-center bg-no-repeat transition-all duration-500 ease-in-out"
+        style={{
+          backgroundImage: `url("${imageUrl}")`,
+        }}
+      />
     </div>
   );
 }
