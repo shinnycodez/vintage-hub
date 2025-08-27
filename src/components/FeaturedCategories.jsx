80c81e0ad6b360274c0c9f15a0ccbb8d71bafb8d
shinnycodez <108819858+shinnycodez@users.noreply.github.com>
2025-08-27 22:41:43 +0500

    Update FeaturedCategories.jsx

--- src/components/FeaturedCategories.jsx
+++ src/components/FeaturedCategories.jsx
@@ -11,7 +11,7 @@ const categories = [
   {
     id: 1,
     title: "Phone charms ⋆｡𖦹°",
-    imageUrl: "https://scontent.flhe7-1.fna.fbcdn.net/v/t1.15752-9/520909658_1678087256927900_4817397154280147514_n.jpg?stp=dst-jpg_s2048x2048_tt6&_nc_cat=101&ccb=1-7&_nc_sid=0024fc&_nc_ohc=-cJamvM0wWoQ7kNvwHjic2W&_nc_oc=AdnGYSG0N4xGE4ybxYKUmZoZaAmTl8C9MNh-ZMSB5zXBe7lCtxIBwzfkg97-6AXqces&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.flhe7-1.fna&oh=03_Q7cD2wGwZvJHpstnpnrNbDdtQrq6BD6DgSYoi2l9j3Ow040fmQ&oe=68AC3F8B",
+    imageUrl: "https://i.postimg.cc/T3mcLkrF/image.png",
       link: "Phone charms",
   },
 
@@ -24,13 +24,13 @@ const categories = [
       {
     id: 2,
     title: "keychains ˚⋆𐙚｡",
-    imageUrl: "https://scontent.flhe3-1.fna.fbcdn.net/v/t1.15752-9/521166975_734660132863062_8344427397155527878_n.jpg?stp=dst-jpg_p480x480_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=_3dWxXDTf2AQ7kNvwH1m5H-&_nc_oc=AdkrOOuB66lOze65nVBUxtzwc30t81Apdwep51coHvlLCe3bNHXBtq_j8SyG158Us5Y&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.flhe3-1.fna&oh=03_Q7cD2wEBc-JVrARW0JcO0ob47QmdjD5VNbxweJm9gTI_uFcHdQ&oe=68AC460B",
+    imageUrl: "https://i.postimg.cc/nhvvcXxt/image.png",
       link: "keychains"
   },
       {
     id: 5,
     title: "Necklaces ｡°✩",
-    imageUrl:"https://scontent.flhe7-2.fna.fbcdn.net/v/t1.15752-9/520429125_1334742338219253_2531984894124566733_n.jpg?stp=dst-jpg_s480x480_tt6&_nc_cat=103&ccb=1-7&_nc_sid=0024fc&_nc_ohc=j7XYMWWmVY8Q7kNvwFYLOQX&_nc_oc=Adkef7qEPDIiflArvyMlAj0FTg2JoPnp6bRd6DV5GSZcJB7m97b8m8ZRWL8id8lVe1k&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.flhe7-2.fna&oh=03_Q7cD2wF-rY8Fqy79pbeR3ML53F8zqAW-4GruhgPAjdrVNB5Twg&oe=68AC16BF",
+    imageUrl:"https://i.postimg.cc/NGST66fc/image.png",
      link: "Necklaces"
   },
         {
